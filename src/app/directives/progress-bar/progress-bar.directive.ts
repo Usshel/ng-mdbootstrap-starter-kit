3746bbd6ec806a6ea9c6572a6d1007d9c4c5163d
Usshel <pawelopadczuk@gmail.com>
2023-05-29 21:48:13 +0200

    feat: color due to progress added

--- src/app/directives/progress-bar/progress-bar.directive.ts
+++ src/app/directives/progress-bar/progress-bar.directive.ts
@@ -1,30 +1,34 @@
-import { Directive, ElementRef, Input, OnChanges, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, Input, Renderer2 } from '@angular/core';
 
 @Directive({ selector: '[progressBarState]' })
-export class ProgressBarDirective implements OnChanges {
-  @Input() progressBarState: number = 0;
-  @Input() progressBarNotDefault: boolean = false;
-  constructor(private _elementRef: ElementRef, private _renderer2: Renderer2) {
-  }
-
-  
-
-
-  ngOnChanges(): void{
-    // const colorMap: Record<number,string> = {
-    //   25:
-    // }
-
+export class ProgressBarDirective {
+  private _progressBarState: number = 0;
+  @Input() set progressBarState(value: number) {
+    this._progressBarState = value;
     this._renderer2.setStyle(
-        this._elementRef.nativeElement, 'width', `${this.progressBarState}%`
-    )
-
-    // if(!this.progressBarNotDefault){
-    //   return this._renderer2.addClass(this._elementRef.nativeElement, `progress-bar ${'bg-primary'}`)
-    // }
-    
+      this._elementRef.nativeElement,
+      'width',
+      `${this._progressBarState}%`
+    );
   }
-  
-
-
+  private _progressBarNotDefault: boolean = false;
+  @Input() set progressBarNotDefault(value: boolean) {
+    this._progressBarNotDefault = value;
+    if (this._progressBarNotDefault && this._progressBarState < 33.33333) {
+      this._renderer2.addClass(this._elementRef.nativeElement, `bg-danger`);
+    } else if (
+      this._progressBarNotDefault &&
+      this._progressBarState < 66.66666
+    ) {
+      this._renderer2.addClass(this._elementRef.nativeElement, `bg-warning`);
+    } else if (
+      this._progressBarNotDefault &&
+      this._progressBarState > 66.66666
+    ) {
+      this._renderer2.addClass(this._elementRef.nativeElement, `bg-success`);
+    } else {
+      this._renderer2.addClass(this._elementRef.nativeElement, `bg-primary`);
+    }
+  }
+  constructor(private _elementRef: ElementRef, private _renderer2: Renderer2) {}
 }
