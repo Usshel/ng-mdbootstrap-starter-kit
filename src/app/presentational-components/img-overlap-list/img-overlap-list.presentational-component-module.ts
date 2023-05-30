b15c41a00a928c6537dd45d598b96dc7d8be9629
Usshel <pawelopadczuk@gmail.com>
2023-05-30 17:04:28 +0200

    feat: dummy component img-list-overlap created

---
+++ src/app/presentational-components/img-overlap-list/img-overlap-list.presentational-component-module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { ImgOverlapListPresentationalComponent } from './img-overlap-list.presentational-component';
+
+@NgModule({
+  imports: [],
+  declarations: [ImgOverlapListPresentationalComponent],
+  providers: [],
+  exports: [ImgOverlapListPresentationalComponent]
+})
+export class ImgOverlapListPresentationalComponentModule {
+}
