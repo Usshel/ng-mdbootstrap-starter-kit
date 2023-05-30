5cdef675c3b730c541c97eb45c9ea58c26e7c096
Usshel <pawelopadczuk@gmail.com>
2023-05-30 21:59:45 +0200

    fix: import added

--- src/app/components/tasks/tasks.component-module.ts
+++ src/app/components/tasks/tasks.component-module.ts
@@ -3,15 +3,21 @@ import { TasksComponent } from './tasks.component';
 import { PageHeaderPresentationalComponentModule } from 'src/app/presentational-components/page-header/page-header.presentational-component-module';
 import { CardPresentationalComponentModule } from 'src/app/presentational-components/card/card.presentational-component-module';
 import { ProgressBarDirectiveModule } from 'src/app/directives/progress-bar/progress-bar.directive-module';
+import { CommonModule } from '@angular/common';
+import { CheckListItemsDonePipeModule } from '../../pipes/check-list-items-done/check-list-items-done.pipe-module';
+import { LengthPipeModule } from '../../pipes/length/length.pipe-module';
 
 @NgModule({
+  declarations: [TasksComponent],
+  providers: [],
+  exports: [TasksComponent],
   imports: [
     PageHeaderPresentationalComponentModule,
     CardPresentationalComponentModule,
     ProgressBarDirectiveModule,
+    CommonModule,
+    CheckListItemsDonePipeModule,
+    LengthPipeModule,
   ],
-  declarations: [TasksComponent],
-  providers: [],
-  exports: [TasksComponent],
 })
 export class TasksComponentModule {}
