76b47b937e0a26a55189c54c0b960abaa83d8dfc
Usshel <pawelopadczuk@gmail.com>
2023-05-31 18:03:10 +0200

    feat: employees component added to routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,27 @@ import { RouterModule, Routes } from '@angular/router';
 import { TeamsComponent } from './components/teams/teams.component';
 import { ProjectsComponent } from './components/projects/projects.component';
 import { TasksComponent } from './components/tasks/tasks.component';
+import { EmployeesComponent } from './components/employees/employees.component';
 import { TeamsComponentModule } from './components/teams/teams.component-module';
 import { ProjectsComponentModule } from './components/projects/projects.component-module';
 import { TasksComponentModule } from './components/tasks/tasks.component-module';
+import { EmployeesComponentModule } from './components/employees/employees.component-module';
 
-const routes: Routes = [{ path: 'teams', component: TeamsComponent }, { path: 'projects', component: ProjectsComponent }, { path: 'tasks', component: TasksComponent }];
+const routes: Routes = [
+  { path: 'teams', component: TeamsComponent },
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'tasks', component: TasksComponent },
+  { path: 'employees', component: EmployeesComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes), TeamsComponentModule, ProjectsComponentModule, TasksComponentModule],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes),
+    TeamsComponentModule,
+    ProjectsComponentModule,
+    TasksComponentModule,
+    EmployeesComponentModule,
+  ],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
