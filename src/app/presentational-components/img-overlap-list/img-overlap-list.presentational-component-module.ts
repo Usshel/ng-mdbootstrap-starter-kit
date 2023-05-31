4941e5174e0169a572b49c0f7ded3562af298fd3
Usshel <pawelopadczuk@gmail.com>
2023-05-31 17:53:19 +0200

    feat: img-overlap-list dummy component extended

--- src/app/presentational-components/img-overlap-list/img-overlap-list.presentational-component-module.ts
+++ src/app/presentational-components/img-overlap-list/img-overlap-list.presentational-component-module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { ImgOverlapListPresentationalComponent } from './img-overlap-list.presentational-component';
+import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
 
 @NgModule({
-  imports: [],
+  imports: [CommonModule, BrowserModule],
   declarations: [ImgOverlapListPresentationalComponent],
   providers: [],
-  exports: [ImgOverlapListPresentationalComponent]
+  exports: [ImgOverlapListPresentationalComponent],
 })
-export class ImgOverlapListPresentationalComponentModule {
-}
+export class ImgOverlapListPresentationalComponentModule {}
--- src/app/presentational-components/img-overlap-list/img-overlap-list.presentational-component.ts
+++ src/app/presentational-components/img-overlap-list/img-overlap-list.presentational-component.ts
@@ -4,14 +4,32 @@ import {
   Input,
   ViewEncapsulation,
 } from '@angular/core';
+import { BehaviorSubject, Observable, map } from 'rxjs';
+import { MembersModel } from 'src/app/models/members.model';
+import { PersonViewModel } from 'src/app/view-models/person/person.view-model';
 
 @Component({
-  selector: 'app-img-overlap-list-presentational',
+  selector: 'img-overlap-list-presentational',
   styleUrls: ['./img-overlap-list.presentational-component.scss'],
   templateUrl: './img-overlap-list.presentational-component.html',
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ImgOverlapListPresentationalComponent {
-  @Input() image!: string;
+  private _persons!: PersonViewModel[];
+  @Input() set persons(value: MembersModel[]) {
+    this._persons! = value.map((member) => ({
+      id: member.id,
+      name: member.firstName,
+      imageUrl: member.avatarUrl,
+      secondName: member.lastName,
+      position: member.position,
+    }));
+    this._personSubject.next(this._persons);
+  }
+  private _personSubject: BehaviorSubject<PersonViewModel[]> =
+    new BehaviorSubject<PersonViewModel[]>(this._persons);
+  public person$: Observable<PersonViewModel[]> =
+    this._personSubject.asObservable();
 }
+// pliki check 1 and 2 .png files from desktop in directory viewModel
