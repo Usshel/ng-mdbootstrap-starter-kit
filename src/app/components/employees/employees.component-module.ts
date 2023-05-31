11da5bc0ba6237f045e678d69450a5827ece2c5d
Usshel <pawelopadczuk@gmail.com>
2023-05-31 21:24:06 +0200

    fix: imports added

--- src/app/components/employees/employees.component-module.ts
+++ src/app/components/employees/employees.component-module.ts
@@ -1,9 +1,17 @@
 import { NgModule } from '@angular/core';
 import { EmployeesComponent } from './employees.component';
 import { PageHeaderPresentationalComponentModule } from 'src/app/presentational-components/page-header/page-header.presentational-component-module';
+import { AvatarCardPresentationalComponentModule } from 'src/app/presentational-components/avatar-card/avatar-card.presentational-component-module';
+import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
 
 @NgModule({
-  imports: [PageHeaderPresentationalComponentModule],
+  imports: [
+    CommonModule,
+    BrowserModule,
+    PageHeaderPresentationalComponentModule,
+    AvatarCardPresentationalComponentModule,
+  ],
   declarations: [EmployeesComponent],
   providers: [],
   exports: [EmployeesComponent],
