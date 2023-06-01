dd9028851679b365ad6e16d1e65aea8b6bfa1dd1
Usshel <pawelopadczuk@gmail.com>
2023-06-01 18:20:59 +0200

    feat:home component added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,16 +4,19 @@ import { TeamsComponent } from './components/teams/teams.component';
 import { ProjectsComponent } from './components/projects/projects.component';
 import { TasksComponent } from './components/tasks/tasks.component';
 import { EmployeesComponent } from './components/employees/employees.component';
+import { HomeComponent } from './components/home/home.component';
 import { TeamsComponentModule } from './components/teams/teams.component-module';
 import { ProjectsComponentModule } from './components/projects/projects.component-module';
 import { TasksComponentModule } from './components/tasks/tasks.component-module';
 import { EmployeesComponentModule } from './components/employees/employees.component-module';
+import { HomeComponentModule } from './components/home/home.component-module';
 
 const routes: Routes = [
   { path: 'teams', component: TeamsComponent },
   { path: 'projects', component: ProjectsComponent },
   { path: 'tasks', component: TasksComponent },
   { path: 'employees', component: EmployeesComponent },
+  { path: '', component: HomeComponent },
 ];
 
 @NgModule({
@@ -23,6 +26,7 @@ const routes: Routes = [
     ProjectsComponentModule,
     TasksComponentModule,
     EmployeesComponentModule,
+    HomeComponentModule,
   ],
   exports: [RouterModule],
 })
