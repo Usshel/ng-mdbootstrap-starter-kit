105c839bc59f97da4b95c0622d58966a9b0f8d20
Usshel <pawelopadczuk@gmail.com>
2023-06-02 11:53:55 +0200

    feat: employee component created

---
+++ src/app/components/employee/employee.component-module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { EmployeeComponent } from './employee.component';
+
+@NgModule({
+  imports: [],
+  declarations: [EmployeeComponent],
+  providers: [],
+  exports: [EmployeeComponent]
+})
+export class EmployeeComponentModule {
+}
