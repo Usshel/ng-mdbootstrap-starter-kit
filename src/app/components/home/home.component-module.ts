89ea89e60191f540670cc162ff5b73bd06006333
Usshel <pawelopadczuk@gmail.com>
2023-06-02 11:33:49 +0200

    feat: imports added

--- src/app/components/home/home.component-module.ts
+++ src/app/components/home/home.component-module.ts
@@ -1,11 +1,23 @@
 import { NgModule } from '@angular/core';
 import { HomeComponent } from './home.component';
+import { PageHeaderPresentationalComponentModule } from 'src/app/presentational-components/page-header/page-header.presentational-component-module';
+import { CardPresentationalComponentModule } from 'src/app/presentational-components/card/card.presentational-component-module';
+import { LengthPipeModule } from 'src/app/pipes/length/length.pipe-module';
+import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+import { AvatarCardPresentationalComponentModule } from 'src/app/presentational-components/avatar-card/avatar-card.presentational-component-module';
 
 @NgModule({
-  imports: [],
+  imports: [
+    PageHeaderPresentationalComponentModule,
+    CardPresentationalComponentModule,
+    LengthPipeModule,
+    CommonModule,
+    BrowserModule,
+    AvatarCardPresentationalComponentModule,
+  ],
   declarations: [HomeComponent],
   providers: [],
-  exports: [HomeComponent]
+  exports: [HomeComponent],
 })
-export class HomeComponentModule {
-}
+export class HomeComponentModule {}
