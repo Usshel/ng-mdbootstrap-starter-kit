be07371328af12a2dd89511c9123530100306dcc
Usshel <pawelopadczuk@gmail.com>
2023-06-02 12:04:35 +0200

    feat: pointer directive created

---
+++ src/app/directives/pointer/pointer.directive-module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { PointerDirective } from './pointer.directive';
+
+@NgModule({
+  imports: [],
+  declarations: [PointerDirective],
+  providers: [],
+  exports: [PointerDirective]
+})
+export class PointerDirectiveModule {
+}
