a29edacf0738d1193eb2907d1e479a621c73ca3a
Usshel <pawelopadczuk@gmail.com>
2023-06-03 10:16:13 +0200

    feat: team component added to router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,12 +6,14 @@ import { TasksComponent } from './components/tasks/tasks.component';
 import { EmployeesComponent } from './components/employees/employees.component';
 import { HomeComponent } from './components/home/home.component';
 import { EmployeeComponent } from './components/employee/employee.component';
+import { TeamComponent } from './components/team/team.component';
 import { TeamsComponentModule } from './components/teams/teams.component-module';
 import { ProjectsComponentModule } from './components/projects/projects.component-module';
 import { TasksComponentModule } from './components/tasks/tasks.component-module';
 import { EmployeesComponentModule } from './components/employees/employees.component-module';
 import { HomeComponentModule } from './components/home/home.component-module';
 import { EmployeeComponentModule } from './components/employee/employee.component-module';
+import { TeamComponentModule } from './components/team/team.component-module';
 
 const routes: Routes = [
   { path: 'teams', component: TeamsComponent },
@@ -20,6 +22,7 @@ const routes: Routes = [
   { path: 'employees', component: EmployeesComponent },
   { path: '', component: HomeComponent },
   { path: 'employee/:employeeId', component: EmployeeComponent },
+  { path: 'teams/:teamId', component: TeamComponent }
 ];
 
 @NgModule({
@@ -31,7 +34,8 @@ const routes: Routes = [
     EmployeesComponentModule,
     HomeComponentModule,
     EmployeeComponentModule,
+    TeamComponentModule
   ],
   exports: [RouterModule],
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
