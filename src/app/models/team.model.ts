76f9187a53ec805c495f1bea9843540b1940b6af
Usshel <pawelopadczuk@gmail.com>
2023-06-04 20:18:06 +0200

    fix: reaadonly to id in TeamModel added

--- src/app/models/team.model.ts
+++ src/app/models/team.model.ts
@@ -1,10 +1,10 @@
-import { MembersModel } from "./members.model";
-import { ProjectModel } from "./project.model";
+import { MembersModel } from './members.model';
+import { ProjectModel } from './project.model';
 
 export interface TeamModel {
   readonly name: string;
   readonly description: string;
-  readonly members: MembersModel[],
-  readonly projects: ProjectModel[],
-  id:string
+  readonly members: MembersModel[];
+  readonly projects: ProjectModel[];
+  readonly id: string;
 }
