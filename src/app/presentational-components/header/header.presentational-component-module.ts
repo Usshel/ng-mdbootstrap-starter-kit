d1ae40841be88d9bbaf2b331abcef3cb75025dd2
Usshel <pawelopadczuk@gmail.com>
2023-06-04 19:05:23 +0200

    feat:  RouterModule to imports added

--- src/app/presentational-components/header/header.presentational-component-module.ts
+++ src/app/presentational-components/header/header.presentational-component-module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { HeaderPresentationalComponent } from './header.presentational-component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  imports: [],
+  imports: [RouterModule],
   declarations: [HeaderPresentationalComponent],
   providers: [],
-  exports: [HeaderPresentationalComponent]
+  exports: [HeaderPresentationalComponent],
 })
-export class HeaderPresentationalComponentModule {
-}
+export class HeaderPresentationalComponentModule {}
