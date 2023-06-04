74579d50ccf675b027b3689606c362dd1f40965f
Usshel <pawelopadczuk@gmail.com>
2023-06-04 20:22:21 +0200

    fix: pointer directive done

--- src/app/directives/pointer/pointer.directive.ts
+++ src/app/directives/pointer/pointer.directive.ts
@@ -1,5 +1,17 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostListener, Renderer2 } from '@angular/core';
 
-@Directive({ selector: '[appPointer]' })
+@Directive({ selector: '[hoverPointer]' })
 export class PointerDirective {
+  constructor(private _renderer2: Renderer2, private _elementRef: ElementRef) {}
+
+  @HostListener('mouseover') onMouseOver() {
+    this._renderer2.setStyle(
+      this._elementRef.nativeElement,
+      'cursor',
+      'pointer'
+    );
+  }
+  @HostListener('mouseleave') onMouseLeave() {
+    this._renderer2.removeStyle(this._elementRef.nativeElement, 'cursor');
+  }
 }
