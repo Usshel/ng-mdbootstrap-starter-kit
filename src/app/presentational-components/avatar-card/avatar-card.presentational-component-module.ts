1fc488570982abe3622b2afa7d49d01cb2da5c5a
Usshel <pawelopadczuk@gmail.com>
2023-06-04 20:15:36 +0200

    feat: imports added to Module

--- src/app/presentational-components/avatar-card/avatar-card.presentational-component-module.ts
+++ src/app/presentational-components/avatar-card/avatar-card.presentational-component-module.ts
@@ -2,9 +2,16 @@ import { NgModule } from '@angular/core';
 import { AvatarCardPresentationalComponent } from './avatar-card.presentational-component';
 import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
+import { EmployeeLinkDirectiveModule } from 'src/app/directives/employee-link/employee-link.directive-module';
+import { PointerDirectiveModule } from 'src/app/directives/pointer/pointer.directive-module';
 
 @NgModule({
-  imports: [CommonModule, BrowserModule],
+  imports: [
+    CommonModule,
+    BrowserModule,
+    EmployeeLinkDirectiveModule,
+    PointerDirectiveModule,
+  ],
   declarations: [AvatarCardPresentationalComponent],
   providers: [],
   exports: [AvatarCardPresentationalComponent],
