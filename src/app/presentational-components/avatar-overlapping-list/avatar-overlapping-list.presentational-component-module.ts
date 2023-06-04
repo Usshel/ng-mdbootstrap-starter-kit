c6f2e2aea56ce4836f9b6659fc3a587e129d5ca8
Usshel <pawelopadczuk@gmail.com>
2023-06-04 19:10:21 +0200

    feat: imports added to avatar module

--- src/app/presentational-components/avatar-overlapping-list/avatar-overlapping-list.presentational-component-module.ts
+++ src/app/presentational-components/avatar-overlapping-list/avatar-overlapping-list.presentational-component-module.ts
@@ -2,9 +2,16 @@ import { NgModule } from '@angular/core';
 import { AvatarOverlappingListPresentationalComponent } from './avatar-overlapping-list.presentational-component';
 import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
+import { PointerDirectiveModule } from 'src/app/directives/pointer/pointer.directive-module';
+import { EmployeeLinkDirectiveModule } from 'src/app/directives/employee-link/employee-link.directive-module';
 
 @NgModule({
-  imports: [CommonModule, BrowserModule],
+  imports: [
+    CommonModule,
+    BrowserModule,
+    PointerDirectiveModule,
+    EmployeeLinkDirectiveModule,
+  ],
   declarations: [AvatarOverlappingListPresentationalComponent],
   providers: [],
   exports: [AvatarOverlappingListPresentationalComponent],
