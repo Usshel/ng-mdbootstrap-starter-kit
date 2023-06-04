420df240445f773e832c16202757bd82f1fbc2d6
Usshel <pawelopadczuk@gmail.com>
2023-06-04 20:23:36 +0200

    feat: imporst added to Module

--- src/app/components/teams/teams.component-module.ts
+++ src/app/components/teams/teams.component-module.ts
@@ -4,6 +4,8 @@ import { PageHeaderPresentationalComponentModule } from 'src/app/presentational-
 import { CardPresentationalComponentModule } from 'src/app/presentational-components/card/card.presentational-component-module';
 import { CommonModule } from '@angular/common';
 import { LengthPipeModule } from 'src/app/pipes/length/length.pipe-module';
+import { PointerDirectiveModule } from 'src/app/directives/pointer/pointer.directive-module';
+import { TeamLinkDirectiveModule } from 'src/app/directives/team-link/team-link.directive-module';
 
 @NgModule({
   declarations: [TeamsComponent],
@@ -14,6 +16,8 @@ import { LengthPipeModule } from 'src/app/pipes/length/length.pipe-module';
     PageHeaderPresentationalComponentModule,
     CardPresentationalComponentModule,
     LengthPipeModule,
+    PointerDirectiveModule,
+    TeamLinkDirectiveModule,
   ],
 })
 export class TeamsComponentModule {}
